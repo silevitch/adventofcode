9fa1f80625c6db8fc8080f0c6f411c8ba3283029
Paul Silevitch <paul@silevitch.com>
2021-12-12 08:07:58 -0500

    added day 12

---
+++ 2021/12/part1.py
@@ -0,0 +1,41 @@
+file = open("input.txt", "r")
+
+caves = {}
+
+for line in file:
+    data = line.strip().split('-')
+
+    if data[0] != 'end':
+        if data[0] in caves:
+            caves[ data[0] ].append( data[1] )
+        else:
+            caves[ data[0] ] = [ data[1] ]
+   
+    if data[0] != 'start':
+        if data[1] in caves:
+            caves[ data[1] ].append( data[0] )
+        else:
+            caves[ data[1] ] = [ data[0] ]
+
+completed = []
+
+def do_path(path, new):
+    new_path = path.copy()
+    if new == 'end':
+        new_path.append(new)
+        completed.append(new_path)
+        return
+
+    if new == new.lower():
+        for cave in new_path:
+            if cave == new:
+                return
+
+    new_path.append( new )
+    
+    for cave in caves[ new ]:
+        do_path(new_path, cave)
+
+do_path([], 'start')
+
+print(len(completed))
